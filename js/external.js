a675091394c92334013f3dfefa1dfa85574afa07
Amida Fombutu <amida.fombutu@gmail.com>
2022-07-28 16:37:19 -0500

    more exercise using exercise 3 from intro lecture

--- js/external.js
+++ js/external.js
@@ -15,15 +15,25 @@ var hercules = prompt("how many days do you want to rent 'Hercules'");
 var pOfLittleMermaid = littleMermaid * 3;
 var pOfBrotherBear = brotherBear * 3;
 var pOfHercules = hercules * 3;
-alert("your total is" + " " + (pOfBrotherBear+pOfHercules+pOfLittleMermaid) +"$" );
-
+alert("your total is "+ "$" +(pOfBrotherBear+pOfHercules+pOfLittleMermaid));
     //EXERCISE 3B
-var googleHrs = prompt("how many hours did u work for Google")
-var amazonHrs = prompt("how many hours did u work for Amazon")
-var facebookHrs = prompt("how many hours did u work for Facebook")
+var googleHrs = prompt("how many hours did u work for Google");
+var amazonHrs = prompt("how many hours did u work for Amazon");
+var facebookHrs = prompt("how many hours did u work for Facebook");
 
-var googlePay = googleHrs * 400
-var amazonPay = amazonHrs * 380
-var facebookPay = facebookHrs * 350
-alert("your total pay is " + (googlePay + amazonPay + facebookPay) + "$")
+var googlePay = googleHrs * 400;
+var amazonPay = amazonHrs * 380;
+var facebookPay = facebookHrs * 350;
+alert("your total pay is " + (googlePay + amazonPay + facebookPay) + "$");
 
+     //Exercise 3c
+var isNotFull = confirm("click OK for Class Availabily else CANCEL");
+var nonConflicting =confirm("click OK for non-confliction else CANCEL ");
+var eligibility = isNotFull && nonConflicting ;
+alert("Eligible for Enrolment:  " + eligibility);
+
+   //Exercise 3d
+var eligibleQuantity = confirm("click OK for more than 2 product bought else CANCEL");
+var productOffer = confirm("Click Ok for unexpired offer else CANCEl");
+var discount = eligibleQuantity && productOffer;
+var discountStatus = alert("Discount Eligibility: "+ discount);
