3105158ad9c887bf8400c50c8f7c6a117ce94362
Amida Fombutu <amida.fombutu@gmail.com>
2022-09-02 18:10:42 -0500

    jquery exercises

--- Class exercises/jquerry exercises/jquerystart.js
+++ Class exercises/jquerry exercises/jquerystart.js
@@ -1,3 +1,4 @@
 $(function() {
     alert( 'The DOM has finished loading!' );
 });
+$("h1").css("background-color","lightgrey").css( "color","blue")
---
+++ js/jquery_exercises.js
@@ -0,0 +1,10 @@
+// $(function () {
+//     alert('The DOM has finished loading!')})
+
+$("ul").css("color","darkgrey").css("text-decoration","underline")
+$("body").css("background-color", "cream-white").css("font-family","Courier New").css("border","1px solid silver")
+$(".codeup").css("color","skyblue").css("border", "1px solid red")
+
+$( "#main,.toop").css("color", "blue")
+$("li").css("font-size", "20px")
+$("li, p, h1").css("font-weight", "900").css("background-color", "beige")
