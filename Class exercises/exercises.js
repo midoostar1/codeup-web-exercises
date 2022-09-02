f580502e995b69fcfebf3c763eeae2a2f1c5e9e0
Amida Fombutu <amida.fombutu@gmail.com>
2022-09-01 23:48:13 -0500

    random qoute generator

---
+++ Class exercises/exercises.js
@@ -0,0 +1,31 @@
+
+var miles = 0
+while(miles<=100){
+  console.log(`the speed of the car is ${miles} per hours`)
+  miles+=10
+}
+
+var a = 1
+var b = 10
+do{
+   console.log(a)
+    a++
+}while(a<=b)
+
+
+var age= 19
+if (age > 20){
+    console.log(`too old`)
+}else{console.log(`not qualified`)}
+
+age>20? console.log(`u are good`): console.log(`not good`)
+
+function sum(){
+
+}var sum = ()=>{
+
+}
+array = [1, 2, 3, 4, 5]
+array.forEach((value, index)=>{
+    console.log(`the item at index ${index} is ${value}`)
+})
