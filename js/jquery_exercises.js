6e480659c65f5e320e3bb1a7a0e781081957bc33
Amida Fombutu <amida.fombutu@gmail.com>
2022-09-06 10:01:44 -0500

    more

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,10 +1,15 @@
 // $(function () {
 //     alert('The DOM has finished loading!')})
-
+alert($(".toop").html())
 $("ul").css("color","darkgrey").css("text-decoration","underline")
 $("body").css("background-color", "cream-white").css("font-family","Courier New").css("border","1px solid silver")
 $(".codeup").css("color","skyblue").css("border", "1px solid red")
 
 $( "#main,.toop").css("color", "blue")
 $("li").css("font-size", "20px")
+$("h1").css("font-weight", "900").css("background-color", "beige")
+$(" p").css("font-weight", "900").css("background-color", "beige")
+$("li").css("font-weight", "900").css("background-color", "beige")
+
+alert($("h1").html())
 $("li, p, h1").css("font-weight", "900").css("background-color", "beige")
